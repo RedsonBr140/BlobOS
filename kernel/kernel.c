565a1c761ef0f3c9cbf1ecb2dd05ecf75fa2c190
Redson <redson@riseup.net>
2023-01-13 21:03:36 -0300

    feat: Scroll support.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,7 +3,5 @@
 void kernel_main(void) {
   /* Initialize terminal interface */
   terminal_initialize();
-
-  /* Newline support is left as an exercise. */
-  terminal_writestring("Hello, kernel World!\n");
+  terminal_writestring("Hello, BlobOS's kernel here.\n");
 }
--- kernel/terminal.c
+++ kernel/terminal.c
@@ -1,7 +1,6 @@
 #include "include/terminal.h"
 #include <stddef.h>
 #include <stdint.h>
-
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
   return fg | bg << 4;
 }
@@ -33,7 +32,7 @@ void terminal_initialize(void) {
 
   for (size_t y = 0; y < VGA_HEIGHT; y++) {
     for (size_t x = 0; x < VGA_WIDTH; x++) {
-      const size_t index = y * VGA_WIDTH + x;
+      const size_t index = x + VGA_WIDTH * y;
       terminal_buffer[index] = vga_entry(' ', terminal_color);
     }
   }
@@ -46,18 +45,42 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
   terminal_buffer[index] = vga_entry(c, color);
 }
 
+// TODO: Please create a libc dude wtf
+void *memmove(void *dstptr, const void *srcptr, size_t size) {
+  unsigned char *dst = (unsigned char *)dstptr;
+  const unsigned char *src = (const unsigned char *)srcptr;
+  if (dst < src) {
+    for (size_t i = 0; i < size; i++)
+      dst[i] = src[i];
+  } else {
+    for (size_t i = size; i != 0; i--)
+      dst[i - 1] = src[i - 1];
+  }
+  return dstptr;
+}
+
+void terminal_scroll(void) {
+  memmove(terminal_buffer, terminal_buffer + 80, 1999 - 80);
+  for (size_t x = 0; x < VGA_WIDTH; x++) {
+    terminal_buffer[VGA_HEIGHT * VGA_WIDTH + x] =
+        vga_entry(' ', terminal_color);
+  }
+}
+
 void terminal_putchar(char c) {
   if (c == '\n') {
     terminal_row++;
     terminal_column = 0;
     return;
   }
-
   terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+
   if (++terminal_column == VGA_WIDTH) {
     terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT)
+    if (++terminal_row == VGA_HEIGHT) {
+      terminal_scroll();
       terminal_row = 0;
+    }
   }
 }
 
