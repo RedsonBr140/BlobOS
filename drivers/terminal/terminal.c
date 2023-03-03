5fd3f018e112918728e264a123498a815db95b05
Redson <redson@riseup.net>
2023-03-03 07:14:49 -0300

    chore: Indentation

--- drivers/terminal/terminal.c
+++ drivers/terminal/terminal.c
@@ -2,18 +2,18 @@
 #include <stddef.h>
 #include <stdint.h>
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
-  return fg | bg << 4;
+    return fg | bg << 4;
 }
 
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
-  return (uint16_t)uc | (uint16_t)color << 8;
+    return (uint16_t)uc | (uint16_t)color << 8;
 }
 
 size_t strlen(const char *str) {
-  size_t len = 0;
-  while (str[len])
-    len++;
-  return len;
+    size_t len = 0;
+    while (str[len])
+        len++;
+    return len;
 }
 
 static const size_t VGA_WIDTH = 80;
@@ -25,70 +25,70 @@ uint8_t terminal_color;
 uint16_t *terminal_buffer;
 
 void terminal_initialize(void) {
-  terminal_row = 0;
-  terminal_column = 0;
-  terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-  terminal_buffer = (uint16_t *)0xB8000;
+    terminal_row = 0;
+    terminal_column = 0;
+    terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+    terminal_buffer = (uint16_t *)0xB8000;
 
-  for (size_t y = 0; y < VGA_HEIGHT; y++) {
-    for (size_t x = 0; x < VGA_WIDTH; x++) {
-      const size_t index = x + VGA_WIDTH * y;
-      terminal_buffer[index] = vga_entry(' ', terminal_color);
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = x + VGA_WIDTH * y;
+            terminal_buffer[index] = vga_entry(' ', terminal_color);
+        }
     }
-  }
 }
 
 void terminal_setcolor(uint8_t color) { terminal_color = color; }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
-  const size_t index = y * VGA_WIDTH + x;
-  terminal_buffer[index] = vga_entry(c, color);
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = vga_entry(c, color);
 }
 
 // TODO: Please create a libc dude wtf
 void *memmove(void *dstptr, const void *srcptr, size_t size) {
-  unsigned char *dst = (unsigned char *)dstptr;
-  const unsigned char *src = (const unsigned char *)srcptr;
-  if (dst < src) {
-    for (size_t i = 0; i < size; i++)
-      dst[i] = src[i];
-  } else {
-    for (size_t i = size; i != 0; i--)
-      dst[i - 1] = src[i - 1];
-  }
-  return dstptr;
+    unsigned char *dst = (unsigned char *)dstptr;
+    const unsigned char *src = (const unsigned char *)srcptr;
+    if (dst < src) {
+        for (size_t i = 0; i < size; i++)
+            dst[i] = src[i];
+    } else {
+        for (size_t i = size; i != 0; i--)
+            dst[i - 1] = src[i - 1];
+    }
+    return dstptr;
 }
 
 void terminal_scroll(void) {
-  memmove(terminal_buffer, terminal_buffer + 80, 1999 - 80);
-  for (size_t x = 0; x < VGA_WIDTH; x++) {
-    terminal_buffer[VGA_HEIGHT * VGA_WIDTH + x] =
-        vga_entry(' ', terminal_color);
-  }
+    memmove(terminal_buffer, terminal_buffer + 80, 1999 - 80);
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+        terminal_buffer[VGA_HEIGHT * VGA_WIDTH + x] =
+            vga_entry(' ', terminal_color);
+    }
 }
 
 void terminal_putchar(char c) {
-  if (c == '\n') {
-    terminal_row++;
-    terminal_column = 0;
-    return;
-  }
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-
-  if (++terminal_column == VGA_WIDTH) {
-    terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT) {
-      terminal_scroll();
-      terminal_row = 0;
+    if (c == '\n') {
+        terminal_row++;
+        terminal_column = 0;
+        return;
     }
-  }
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+
+    if (++terminal_column == VGA_WIDTH) {
+        terminal_column = 0;
+        if (++terminal_row == VGA_HEIGHT) {
+            terminal_scroll();
+            terminal_row = 0;
+        }
+    }
 }
 
 void terminal_write(const char *data, size_t size) {
-  for (size_t i = 0; i < size; i++)
-    terminal_putchar(data[i]);
+    for (size_t i = 0; i < size; i++)
+        terminal_putchar(data[i]);
 }
 
 void terminal_writestring(const char *data) {
-  terminal_write(data, strlen(data));
+    terminal_write(data, strlen(data));
 }
