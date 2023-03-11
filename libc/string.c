22333b14cc220f5930dbdecbaf67fbf4b051d077
Redson <redson@riseup.net>
2023-03-11 17:58:38 -0300

    feat: strcmp function
    Signed-off-by: Redson <redson@riseup.net>

--- libc/string.c
+++ libc/string.c
@@ -26,3 +26,14 @@ void *memmove(void *dstptr, const void *srcptr, size_t size) {
     }
     return dstptr;
 }
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 == *s2) {
+        if (*s1 == '\0') {
+            return 0;
+        }
+        s1++;
+        s2++;
+    }
+    return (*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : 1;
+}
