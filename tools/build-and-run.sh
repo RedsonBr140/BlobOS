8ba437277283ade1a700ee45a94bb05ce448afde
Redson <redson@riseup.net>
2023-04-14 11:14:44 -0300

    tools: The script will be run manually for the time being.

--- tools/build-and-run.sh
+++ tools/build-and-run.sh
@@ -1,19 +1,20 @@
 #!/bin/env bash
 
-cd ${MESON_SOURCE_ROOT}
+#cd ${MESON_SOURCE_ROOT}
 
 echo -e "===\nScript working directory: $(pwd)\n===\n"
 if [ ! -d "build" ]; then
+    echo "Build doesn't exist."
 	meson setup build
 fi
 
 meson compile -C build
 if [ $1 == "iso" ]; then
 	echo "Running the ISO via qemu-system-i386"
-	qemu-system-i386 -cdrom build/BlobOS.iso
+	qemu-system-x86_64 -cdrom build/BlobOS.iso
 elif [[ $1 == "bin" ]]; then
 	echo "Running the .bin via qemu-system-i386"
-	qemu-system-i386 -kernel build/BlobOS.bin
+	qemu-system-x86_64 -kernel build/BlobOS.bin
 else
 	echo "You screwed up."
 	exit 1
