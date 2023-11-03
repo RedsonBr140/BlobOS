d7da5340795c53e1fc0f42601ef7b835973a4305
Redson <redson@riseup.net>
2023-11-03 09:52:48 -0300

    chore: Clean Up. Starting Interrupts again...
    Signed-off-by: Redson <redson@riseup.net>

--- Kernel/Arch/x86_64-pc/Include/System/PIC.h
+++ Kernel/Arch/x86_64-pc/Include/System/PIC.h
@@ -1,3 +1,33 @@
 #pragma once
 
-void PIC_Init(int PIC1, int PIC2);
+#define PIC_REMAP_OFFSET 0x20
+
+/*
+ * IRQ macros
+ */
+#define ALL 0xFF
+#define TIMER 0
+#define KEYBOARD 1
+#define CASCADE 2
+#define COM2_4 3
+#define COM1_3 4
+#define LPT 5
+#define FLOPPY 6
+#define FREE7 7
+#define CLOCK 8
+#define FREE9 9
+#define FREE10 10
+#define FREE11 11
+#define PS2MOUSE 12
+#define COPROC 13
+#define IDE_1 14
+#define IDE_2 15
+
+/*
+ * Functions
+ */
+
+void PIC_Initialize(int offset1, int offset2);
+void PIC_SendEOI(unsigned char irq);
+void PIC_Mask(unsigned char IRQline);
+void PIC_Unmask(unsigned char IRQline);
--- Kernel/Arch/x86_64-pc/System/PIC.c
+++ Kernel/Arch/x86_64-pc/System/PIC.c
@@ -1,37 +1,89 @@
 #include <IO/Ports.h>
 #include <System/PIC.h>
-#define PIC1 0x20
-#define PIC2 0xA0
 
-#define ICW1 0x11
-#define ICW4 0x01
+#define PIC1 0x20 // Master PIC address
+#define PIC2 0xA0 // Slave PIC address
 
-void PIC_Init(int pic1, int pic2) {
-    // Send ICW1
-    outb(PIC1, ICW1);
+#define PIC1_COMMAND PIC1
+#define PIC1_DATA (PIC1 + 1)
+
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA (PIC2 + 1)
+
+#define PIC_EOI 0x20
+
+/* reinitialize the PIC controllers, giving them specified vector offsets
+   rather than 8h and 70h, as configured by default */
+
+#define ICW1_ICW4 0x01      /* Indicates that ICW4 will be present */
+#define ICW1_SINGLE 0x02    /* Single (cascade) mode */
+#define ICW1_INTERVAL4 0x04 /* Call address interval 4 (8) */
+#define ICW1_LEVEL 0x08     /* Level triggered (edge) mode */
+#define ICW1_INIT 0x10      /* Initialization - required! */
+
+#define ICW4_8086 0x01       /* 8086/88 (MCS-80/85) mode */
+#define ICW4_AUTO 0x02       /* Auto (normal) EOI */
+#define ICW4_BUF_SLAVE 0x08  /* Buffered mode/slave */
+#define ICW4_BUF_MASTER 0x0C /* Buffered mode/master */
+#define ICW4_SFNM 0x10       /* Special fully nested (not) */
+
+void PIC_Initialize(int offset1, int offset2) {
+    outb(PIC1_COMMAND,
+         ICW1_INIT |
+             ICW1_ICW4); // starts the initialization sequence (in cascade mode)
     io_wait();
-    outb(PIC2, ICW1);
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    outb(PIC1_DATA, offset1); // ICW2: Master PIC vector offset
+    io_wait();
+    outb(PIC2_DATA, offset2); // ICW2: Slave PIC vector offset
+    io_wait();
+    outb(PIC1_DATA, 4); // ICW3: tell Master PIC that there is a slave PIC at
+                        // IRQ2 (0000 0100)
+    io_wait();
+    outb(PIC2_DATA, 2); // ICW3: tell Slave PIC its cascade identity (0000 0010)
     io_wait();
 
-    // Send ICW2
-    outb(PIC1 + 1, pic1); // Remap
+    outb(PIC1_DATA,
+         ICW4_8086); // ICW4: have the PICs use 8086 mode (and not 8080 mode)
     io_wait();
-    outb(PIC2 + 1, pic2); // PICs
-    io_wait();
-
-    // Send ICW3
-    outb(PIC1 + 1, 4); // IRQ2 -> connection to slave
-    io_wait();
-    outb(PIC2 + 1, 2);
+    outb(PIC2_DATA, ICW4_8086);
     io_wait();
 
-    // Send ICW4
-    outb(PIC1 + 1, ICW4);
-    io_wait();
-    outb(PIC2 + 1, ICW4);
-    io_wait();
+    outb(PIC1_DATA, 0xFF); // Masks all IRQs.
+}
 
-    // Disable all IRQs
-    outb(PIC1 + 1, 0xFF);
-    io_wait();
+void PIC_Mask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if (IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);
+}
+
+void PIC_Unmask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if (IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);
+}
+
+void PIC_SendEOI(unsigned char irq) {
+    if (irq >= 8)
+        outb(PIC2_COMMAND, PIC_EOI);
+    outb(PIC1_COMMAND, PIC_EOI);
 }
